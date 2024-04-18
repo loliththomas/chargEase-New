310631ed531a037028f269cf3c358515ba8a4103
Lolith Thomas <loliththomas@gmail.com>
2024-04-18 23:38:29 +0530

    otp authentication done

--- chargease/lib/Screens/OtpScreen.dart
+++ chargease/lib/Screens/OtpScreen.dart
@@ -6,31 +6,32 @@ import 'package:chargease/screens/dataentryScreen.dart';
 class OtpScreen extends StatefulWidget {
   final String phoneNumber;
   final int otp;
-  OtpScreen({required this.phoneNumber,required  this.otp});
-  
+  final String verificationId;
+  OtpScreen({required this.phoneNumber, required this.otp,required this.verificationId});
+
   @override
   _OtpScreenState createState() => _OtpScreenState();
 }
 
 class _OtpScreenState extends State<OtpScreen> {
-  String  _otp='' ; // To store the entered OTP
+  String _otp = ''; // To store the entered OTP
   final _formKey = GlobalKey<FormState>();
 
   void _verifyOtp() {
-   if(int.parse(_otp)== widget.otp){
-    // otp matched
-    print('otp matched');
-     Navigator.push(context, 
-     MaterialPageRoute(builder: (context)=> DataEntryScreen(phoneNumber: widget.phoneNumber)),
-     );
-   }
-   else{
-        print('otp missmatch');
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-          content: Text("OTP does not match"),
-        ));
-   }
-    print("Verifying OTP: $_otp");
+    if (_otp == widget.otp.toString()) {
+      // OTP matched
+      print('OTP matched');
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => DataEntryScreen(phoneNumber: widget.phoneNumber)),
+      );
+    } else {
+      // OTP mismatched
+      print('OTP mismatch');
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text("OTP does not match"),
+      ));
+    }
   }
 
   @override
@@ -40,13 +41,13 @@ class _OtpScreenState extends State<OtpScreen> {
         child: Container(
           padding: EdgeInsets.all(20.0),
           child: Column(
-            /*mainAxisAlignment: MainAxisAlignment.center,*/
             children: [
               // Image
               Image.asset(
                 'assets/images/otp.jpg',
-              height: 250,
-              width: 350,), // Replace with your otp icon path
+                height: 250,
+                width: 350,
+              ), // Replace with your otp icon path
               SizedBox(height: 10.0),
 
               // Text - VERIFICATION
@@ -55,17 +56,17 @@ class _OtpScreenState extends State<OtpScreen> {
                 style: GoogleFonts.getFont(
                   "Poppins",
                   textStyle: TextStyle(
-                  color: Color.fromARGB(255, 0, 0, 0),
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                  )  
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
               SizedBox(height: 10.0),
 
               // Text - Enter the otp send to your number
               Text(
-                'Enter the OTP sent to  +91 ${widget.phoneNumber}',
+                'Enter the OTP sent to +91 ${widget.phoneNumber}',
                 style: TextStyle(
                   fontSize: 16.0,
                   color: Color.fromARGB(153, 0, 0, 0),
@@ -84,10 +85,7 @@ class _OtpScreenState extends State<OtpScreen> {
                       labelText: 'OTP',
                       counterText: "",
                       border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Color(0xD9D9D9)
-                  
-                        )
+                        borderSide: BorderSide(color: Color(0xD9D9D9)),
                       ),
                     ),
                     onChanged: (value) {
@@ -95,8 +93,8 @@ class _OtpScreenState extends State<OtpScreen> {
                         _otp = value;
                       });
                     },
-                    validator:(value) {
-                      if (value == null || value.isEmpty){
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
                         return "OTP required";
                       }
                       return null;
@@ -112,7 +110,7 @@ class _OtpScreenState extends State<OtpScreen> {
                 children: [
                   TextButton(
                     onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: ((context)  =>LoginScreen())));
+                      Navigator.push(context, MaterialPageRoute(builder: ((context) => LoginScreen())));
                       // Handle Change Number click
                     },
                     child: Text('Change Number'),
@@ -126,44 +124,45 @@ class _OtpScreenState extends State<OtpScreen> {
                   ),
                 ],
               ),
-              SizedBox(),
+              SizedBox(height: 20.0),
+
+              // Verify Button
               Container(
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: DecoratedBox(
-                        child: SizedBox(
-                          width: 110,
-                          height: 40,
-                          child: ElevatedButton(
-                            onPressed: () {
-                              _formKey.currentState!.validate();
-                              _verifyOtp();
-                            },
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor:
-                                  const Color.fromARGB(0, 255, 255, 255),
-                              // Transparent background to ensure gradient shows
-                            ),
-                            child: Text(
-                              'Verify',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 16,
-                              ),
-                            ),
-                          ),
-                        ),
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                            colors: [
-                              Color.fromARGB(255, 63, 159, 172),
-                              Color.fromARGB(255, 38, 123, 233)
-                            ], // Your desired colors
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: DecoratedBox(
+                    child: SizedBox(
+                      width: 110,
+                      height: 40,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            _verifyOtp();
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(0, 255, 255, 255),
+                        ),// Transparent background to ensure gradient shows
+                        child: Text(
+                          'Verify',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
                           ),
                         ),
                       ),
                     ),
-                  )
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Color.fromARGB(255, 63, 159, 172),
+                          Color.fromARGB(255, 38, 123, 233)
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              )
             ],
           ),
         ),
--- chargease/lib/Screens/loginScreen.dart
+++ chargease/lib/Screens/loginScreen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:chargease/screens/OtpScreen.dart';
 import 'dart:math';
-
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -9,186 +9,156 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  // Error message display state
   String _phoneNumberError = '';
+  late String verificationId; // Declare verificationId here
   late int _otp;
-  final _formKey =GlobalKey<FormState>(); // State management for form validation
+  final _formKey = GlobalKey<FormState>();
   final _phoneController = TextEditingController();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         padding: EdgeInsets.all(20.0),
         child: Form(
-          key: _formKey, // Associate form key for validation
+          key: _formKey,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              Image.asset(
-                'assets/images/logo_name.png', // Replace with your logo path
-              ),
+              Image.asset('assets/images/logo_name.png'),
               SizedBox(height: 20.0),
               TextFormField(
-                keyboardType:
-                    TextInputType.phone, // Optimized input for phone number
+                keyboardType: TextInputType.phone,
                 controller: _phoneController,
-                maxLength: 10, // Limit input length to 10 digits
+                maxLength: 13, // Adjust the length according to E.164 format
                 decoration: InputDecoration(
-                  labelText: 'Phone Number',
+                  labelText: 'Phone Number (+CountryCodeXXXXXXXXX)',
                   counterText: "",
                   prefixIcon: Icon(Icons.phone),
                   errorText: _phoneNumberError.isNotEmpty
                       ? _phoneNumberError
-                      : null, // Display error message if exists
+                      : null,
                 ),
-                /* validator: (value) { // Validation logic
-                  if (value!.isEmpty) {
-                    return 'Phone number is required.';
-                  } else if (value.length < 10) {
-                    setState(() {
-                      _phoneNumberError = 'Phone number must be at least 10 digits.';
-                    });
-                    return null; // Don't return error message to avoid showing 2 simultaneously
-                  } else {
-                    setState(() {
-                      _phoneNumberError = ''; // Clear error message on valid input
-                    });
-                    return null;
-                  }
-                },*/
               ),
               SizedBox(height: 40.0),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: DecoratedBox(
-                        child: SizedBox(
-                          width: 150,
-                          height: 36,
-                          child: ElevatedButton(
-                            onPressed: () {
+              Container(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: DecoratedBox(
+                    child: SizedBox(
+                      width: 150,
+                      height: 36,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          String phoneNumber = _phoneController.text;
+                          _otp = Random().nextInt(900000) + 100000;
+                          print("OTP is $_otp");
 
-                              // Get the phone number
-                              String phoneNumber = _phoneController.text;
+                          if (phoneNumber.isEmpty) {
+                            setState(() {
+                              _phoneNumberError = 'Phone number is required.';
+                            });
+                            return;
+                          } else if (!_isValidPhoneNumber(phoneNumber)) {
+                            setState(() {
+                              _phoneNumberError = 'Invalid phone number format.';
+                            });
+                            return;
+                          }
 
-                              //Generating otp
-                              _otp=Random().nextInt(900000)+100000;
-                              print("OTP is  $_otp");
-                              // Perform validation
-                              if (phoneNumber.isEmpty) {
-                                setState(() {
-                                  _phoneNumberError ='Phone number is required.';
-                                });
-                                return; // Prevent further action if empty
-                              } else if (phoneNumber.length < 10) {
-                                setState(() {
-                                  _phoneNumberError =
-                                      'Phone number must be at least 10 digits.';
-                                });
-                                return; // Prevent further action if invalid length
-                              }
+                          setState(() {
+                            _phoneNumberError = '';
+                          });
 
-                              // Proceed with logic if valid
-                              setState(() {
-                                _phoneNumberError = ''; // Clear error if valid
-                              });
-                              Navigator.push(context, 
-                              MaterialPageRoute(builder: (context) =>OtpScreen(
-                                phoneNumber: phoneNumber,
-                                otp: 123456  /*_otp*/,)
-                                )
+                          // Send OTP to the user's phone number
+                          _auth.verifyPhoneNumber(
+                            phoneNumber: phoneNumber,
+                            verificationCompleted: (PhoneAuthCredential credential) {},
+                            verificationFailed: (FirebaseAuthException e) {},
+                            codeSent: (String vId, int? resendToken) {
+                              verificationId = vId; // Assign verificationId here
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => OtpScreen(
+                                    verificationId: verificationId,
+                                    phoneNumber: phoneNumber,
+                                    otp: 123456/*_otp*/,
+                                  ),
+                                ),
                               );
-                              // Implement your actual login functionality here, potentially using phoneNumber
-                              // ...
                             },
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor:
-                                  const Color.fromARGB(0, 255, 255, 255),
-                              // Transparent background to ensure gradient shows
-                            ),
-                            child: Text(
-                              'Sign up',
-                              style: TextStyle(
-                                color: Colors.white,
-                              ),
-                            ),
-                          ),
+                            codeAutoRetrievalTimeout: (String verificationId) {},
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(0, 255, 255, 255),
                         ),
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                            colors: [
-                              Color.fromARGB(255, 63, 159, 172),
-                              Color.fromARGB(255, 38, 123, 233)
-                            ], // Your desired colors
-                          ),
-                        ),
+                        child: Text('Sign up', style: TextStyle(color: Colors.white)),
                       ),
                     ),
-                  ), // ... other UI elements
-                  SizedBox(height: 20.0),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        child: Divider(
-                          indent: 120,
-                          thickness: 1,
-                          color: Color.fromARGB(255, 115, 111, 111),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10),
-                        child: Text('OR',
-                            style: TextStyle(
-                                color: Color.fromARGB(255, 115, 111, 111))),
-                      ),
-                      Expanded(
-                        child: Divider(
-                          endIndent: 120,
-                          thickness: 1,
-                          color: Color.fromARGB(255, 115, 111, 111),
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 20.0),
-                  SizedBox(
-                    width: 250,
-                    height: 36,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        // Implement login button functionality, considering form validation
-                        if (_formKey.currentState!.validate()) {
-                          // Form is valid, proceed with login process
-                        }
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.transparent,
-                        foregroundColor: Colors.transparent,
-                        side: BorderSide(color: Colors.black, width: 2),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                      ),
-                      child: Row(
-                        children: [
-                          Image.asset("assets/images/google.png", height: 30),
-                          SizedBox(width: 10),
-                          Text(
-                            'Continue with google',
-                            style: TextStyle(
-                              color: Colors.black,
-                            ),
-                          ),
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Color.fromARGB(255, 63, 159, 172),
+                          Color.fromARGB(255, 38, 123, 233)
                         ],
                       ),
                     ),
                   ),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Divider(
+                      indent: 120,
+                      thickness: 1,
+                      color: Color.fromARGB(255, 115, 111, 111),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Text(
+                      'OR',
+                      style: TextStyle(color: Color.fromARGB(255, 115, 111, 111)),
+                    ),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      endIndent: 120,
+                      thickness: 1,
+                      color: Color.fromARGB(255, 115, 111, 111),
+                    ),
+                  ),
                 ],
+              ),
+              SizedBox(height: 20.0),
+              SizedBox(
+                width: 250,
+                height: 36,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    foregroundColor: Colors.transparent,
+                    side: BorderSide(color: Colors.black, width: 2),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  child: Row(
+                    children: [
+                      Image.asset("assets/images/google.png", height: 30),
+                      SizedBox(width: 10),
+                      Text('Continue with Google', style: TextStyle(color: Colors.black)),
+                    ],
+                  ),
+                ),
               ),
             ],
           ),
@@ -196,4 +166,17 @@ class _LoginScreenState extends State<LoginScreen> {
       ),
     );
   }
+
+  bool _isValidPhoneNumber(String value) {
+    // Implement validation logic here for E.164 format
+    // Example validation: Check if value starts with '+' and contains only digits after that
+    if (value.startsWith('+') && value.substring(1).contains(RegExp(r'^[0-9]*$'))) {
+      return true;
+    }
+    return false;
+  }
 }
+
+
+
+//currently firebase il oru testingin vendi my num kodthitund + verification code set to 123456
